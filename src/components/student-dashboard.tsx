0dafc5f63ad481be8ba81453bd875bdbc2cc4231
Cetc Topete <mpceciliotopetecruz@gmail.com>
2025-12-19 04:01:09 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/student-dashboard.tsx
+++ src/components/student-dashboard.tsx
@@ -89,7 +89,7 @@ const StudentDashboard: React.FC = () => {
                                         />
                                     </td>
                                     <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
-                                        <Link href={linkHref} passHref legacyBehavior>
+                                        <Link href={linkHref} asChild>
                                              <Button
                                                 variant="default"
                                                 size="sm"
